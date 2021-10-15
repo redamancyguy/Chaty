ff8ccfb49175a67e24751e3d964ccf2151667cd3
1506607292 <70865034+1506607292@users.noreply.github.com>
2021-10-15 17:09:06 +0800

    kernel
    addCpuSet
    AddthreadPrivileges

--- core/cmake-build-debug/CMakeFiles/core.dir/DependInfo.cmake
+++ core/cmake-build-debug/CMakeFiles/core.dir/DependInfo.cmake
@@ -1,23 +1,15 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
   "C"
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/sunwenli/CLionProjects/core/main.c" "/home/sunwenli/CLionProjects/core/cmake-build-debug/CMakeFiles/core.dir/main.c.o"
+  "/mnt/c/Users/sunwenli/Documents/GitHub/ChatTogether/core/main.c" "/mnt/c/Users/sunwenli/Documents/GitHub/ChatTogether/core/cmake-build-debug/CMakeFiles/core.dir/main.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
 # The include file search paths:
 set(CMAKE_C_TARGET_INCLUDE_PATH
-  )
-
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
   )
 
 # Targets to which this target links.
