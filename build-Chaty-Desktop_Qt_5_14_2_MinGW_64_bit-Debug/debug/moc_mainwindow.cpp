1e5927675bed3c1d4725d9657e4d52f9c3d3561e
1506607292 <70865034+1506607292@users.noreply.github.com>
2021-10-23 13:33:23 +0800

    111

--- build-Chaty-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
+++ build-Chaty-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -7,7 +7,7 @@
 *****************************************************************************/
 
 #include <memory>
-#include "../../QTclient/mainwindow.h"
+#include "../../../../../Desktop/Chaty/mainwindow.h"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
@@ -22,8 +22,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[11];
-    char stringdata0[147];
+    QByteArrayData data[8];
+    char stringdata0[82];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -39,17 +39,12 @@ QT_MOC_LITERAL(3, 23, 12), // "QCloseEvent*"
 QT_MOC_LITERAL(4, 36, 5), // "event"
 QT_MOC_LITERAL(5, 42, 21), // "on_listWidget_clicked"
 QT_MOC_LITERAL(6, 64, 11), // "QModelIndex"
-QT_MOC_LITERAL(7, 76, 5), // "index"
-QT_MOC_LITERAL(8, 82, 23), // "on_activatedSysTrayIcon"
-QT_MOC_LITERAL(9, 106, 33), // "QSystemTrayIcon::ActivationRe..."
-QT_MOC_LITERAL(10, 140, 6) // "reason"
+QT_MOC_LITERAL(7, 76, 5) // "index"
 
     },
     "MainWindow\0closeEvent\0\0QCloseEvent*\0"
     "event\0on_listWidget_clicked\0QModelIndex\0"
-    "index\0on_activatedSysTrayIcon\0"
-    "QSystemTrayIcon::ActivationReason\0"
-    "reason"
+    "index"
 };
 #undef QT_MOC_LITERAL
 
@@ -59,7 +54,7 @@ static const uint qt_meta_data_MainWindow[] = {
        8,       // revision
        0,       // classname
        0,    0, // classinfo
-       3,   14, // methods
+       2,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -67,14 +62,12 @@ static const uint qt_meta_data_MainWindow[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    1,   29,    2, 0x09 /* Protected */,
-       5,    1,   32,    2, 0x08 /* Private */,
-       8,    1,   35,    2, 0x08 /* Private */,
+       1,    1,   24,    2, 0x09 /* Protected */,
+       5,    1,   27,    2, 0x08 /* Private */,
 
  // slots: parameters
     QMetaType::Void, 0x80000000 | 3,    4,
     QMetaType::Void, 0x80000000 | 6,    7,
-    QMetaType::Void, 0x80000000 | 9,   10,
 
        0        // eod
 };
@@ -87,7 +80,6 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         switch (_id) {
         case 0: _t->closeEvent((*reinterpret_cast< QCloseEvent*(*)>(_a[1]))); break;
         case 1: _t->on_listWidget_clicked((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
-        case 2: _t->on_activatedSysTrayIcon((*reinterpret_cast< QSystemTrayIcon::ActivationReason(*)>(_a[1]))); break;
         default: ;
         }
     }
@@ -122,13 +114,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 3)
+        if (_id < 2)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 3;
+        _id -= 2;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 3)
+        if (_id < 2)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 3;
+        _id -= 2;
     }
     return _id;
 }
--- build-Chaty-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-Chaty-Desktop_Qt_5_14_2_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -10,12 +10,17 @@
 #define UI_MAINWINDOW_H
 
 #include <QtCore/QVariant>
+#include <QtGui/QIcon>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QHBoxLayout>
 #include <QtWidgets/QListWidget>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QSpacerItem>
+#include <QtWidgets/QSplitter>
 #include <QtWidgets/QTextBrowser>
 #include <QtWidgets/QTextEdit>
+#include <QtWidgets/QVBoxLayout>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -24,68 +29,147 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QTextBrowser *textBrowser;
-    QTextEdit *textEdit;
+    QHBoxLayout *horizontalLayout_4;
+    QSplitter *list_splitter;
     QListWidget *listWidget;
-    QStatusBar *statusbar;
+    QSplitter *input_splitter;
+    QTextBrowser *textBrowser;
+    QWidget *widget;
+    QVBoxLayout *verticalLayout;
+    QTextEdit *textEdit;
+    QHBoxLayout *horizontalLayout_3;
+    QSpacerItem *horizontalSpacer;
+    QPushButton *send;
 
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(800, 413);
+        MainWindow->resize(1033, 704);
         MainWindow->setMinimumSize(QSize(800, 413));
+        QIcon icon;
+        icon.addFile(QString::fromUtf8(":/chaty.ico"), QSize(), QIcon::Normal, QIcon::Off);
+        MainWindow->setWindowIcon(icon);
+        MainWindow->setStyleSheet(QString::fromUtf8("QMainWindow{\n"
+"	background-color: rgb(255, 255, 255);\n"
+"}\n"
+"font: 9pt \"Arial\";"));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
-        textBrowser = new QTextBrowser(centralwidget);
+        horizontalLayout_4 = new QHBoxLayout(centralwidget);
+        horizontalLayout_4->setSpacing(0);
+        horizontalLayout_4->setObjectName(QString::fromUtf8("horizontalLayout_4"));
+        horizontalLayout_4->setContentsMargins(0, 0, 0, 0);
+        list_splitter = new QSplitter(centralwidget);
+        list_splitter->setObjectName(QString::fromUtf8("list_splitter"));
+        list_splitter->setOrientation(Qt::Horizontal);
+        list_splitter->setChildrenCollapsible(false);
+        listWidget = new QListWidget(list_splitter);
+        listWidget->setObjectName(QString::fromUtf8("listWidget"));
+        QSizePolicy sizePolicy(QSizePolicy::Minimum, QSizePolicy::Expanding);
+        sizePolicy.setHorizontalStretch(0);
+        sizePolicy.setVerticalStretch(0);
+        sizePolicy.setHeightForWidth(listWidget->sizePolicy().hasHeightForWidth());
+        listWidget->setSizePolicy(sizePolicy);
+        listWidget->setStyleSheet(QString::fromUtf8(""));
+        list_splitter->addWidget(listWidget);
+        input_splitter = new QSplitter(list_splitter);
+        input_splitter->setObjectName(QString::fromUtf8("input_splitter"));
+        QSizePolicy sizePolicy1(QSizePolicy::Maximum, QSizePolicy::Expanding);
+        sizePolicy1.setHorizontalStretch(0);
+        sizePolicy1.setVerticalStretch(0);
+        sizePolicy1.setHeightForWidth(input_splitter->sizePolicy().hasHeightForWidth());
+        input_splitter->setSizePolicy(sizePolicy1);
+        input_splitter->setOrientation(Qt::Vertical);
+        input_splitter->setChildrenCollapsible(false);
+        textBrowser = new QTextBrowser(input_splitter);
         textBrowser->setObjectName(QString::fromUtf8("textBrowser"));
-        textBrowser->setGeometry(QRect(110, 20, 691, 251));
+        QSizePolicy sizePolicy2(QSizePolicy::Expanding, QSizePolicy::Expanding);
+        sizePolicy2.setHorizontalStretch(0);
+        sizePolicy2.setVerticalStretch(0);
+        sizePolicy2.setHeightForWidth(textBrowser->sizePolicy().hasHeightForWidth());
+        textBrowser->setSizePolicy(sizePolicy2);
         textBrowser->setStyleSheet(QString::fromUtf8("QWidget{\n"
 "	color:rgb(150,0,150);\n"
 "	font:12pt \"Arial Black\";\n"
-"    background-color:rgb(255,225,225);\n"
+"    background-color:#F5F6F7;\n"
 "	font-family: Source Sans Pro; \n"
 "    font-weight: bold; \n"
+"	border:none;\n"
 "}\n"
 ""));
-        textEdit = new QTextEdit(centralwidget);
+        input_splitter->addWidget(textBrowser);
+        widget = new QWidget(input_splitter);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        verticalLayout = new QVBoxLayout(widget);
+        verticalLayout->setSpacing(0);
+        verticalLayout->setObjectName(QString::fromUtf8("verticalLayout"));
+        verticalLayout->setSizeConstraint(QLayout::SetMinimumSize);
+        verticalLayout->setContentsMargins(0, 0, 0, 0);
+        textEdit = new QTextEdit(widget);
         textEdit->setObjectName(QString::fromUtf8("textEdit"));
-        textEdit->setGeometry(QRect(110, 270, 691, 121));
-        textEdit->setStyleSheet(QString::fromUtf8("QWidget{\n"
-"	font:12pt \"Arial Black\";\n"
-"	font-family: Source Sans Pro; \n"
-"    font-weight: bold; \n"
-"    background-color:beige;\n"
-"    color:rgb(150,0,150);\n"
+        sizePolicy2.setHeightForWidth(textEdit->sizePolicy().hasHeightForWidth());
+        textEdit->setSizePolicy(sizePolicy2);
+        QFont font;
+        font.setPointSize(12);
+        textEdit->setFont(font);
+        textEdit->setStyleSheet(QString::fromUtf8("QTextEdit{\n"
+"	border:none;\n"
 "}"));
-        listWidget = new QListWidget(centralwidget);
-        listWidget->setObjectName(QString::fromUtf8("listWidget"));
-        listWidget->setGeometry(QRect(0, 20, 111, 371));
-        listWidget->setStyleSheet(QString::fromUtf8("QWidget{\n"
-"	font:9pt \"Arial Black\";\n"
-"	font-family: Source Sans Pro; \n"
-"    font-weight: bold; \n"
-"    background-color:rgb(214,214,214);\n"
-"    color:rgb(255,0,100);\n"
+
+        verticalLayout->addWidget(textEdit);
+
+        horizontalLayout_3 = new QHBoxLayout();
+        horizontalLayout_3->setObjectName(QString::fromUtf8("horizontalLayout_3"));
+        horizontalLayout_3->setSizeConstraint(QLayout::SetMinimumSize);
+        horizontalSpacer = new QSpacerItem(40, 20, QSizePolicy::Expanding, QSizePolicy::Minimum);
+
+        horizontalLayout_3->addItem(horizontalSpacer);
+
+        send = new QPushButton(widget);
+        send->setObjectName(QString::fromUtf8("send"));
+        QFont font1;
+        font1.setPointSize(10);
+        send->setFont(font1);
+        send->setAutoFillBackground(false);
+        send->setStyleSheet(QString::fromUtf8("QPushButton{\n"
+"	margin:5px;\n"
+"	height:30px;\n"
+"	color: rgb(30, 111, 255);\n"
+"	width:80px;\n"
 "}"));
+        send->setAutoDefault(false);
+        send->setFlat(false);
+
+        horizontalLayout_3->addWidget(send);
+
+
+        verticalLayout->addLayout(horizontalLayout_3);
+
+        input_splitter->addWidget(widget);
+        list_splitter->addWidget(input_splitter);
+
+        horizontalLayout_4->addWidget(list_splitter);
+
         MainWindow->setCentralWidget(centralwidget);
-        statusbar = new QStatusBar(MainWindow);
-        statusbar->setObjectName(QString::fromUtf8("statusbar"));
-        MainWindow->setStatusBar(statusbar);
 
         retranslateUi(MainWindow);
+
+        send->setDefault(false);
+
 
         QMetaObject::connectSlotsByName(MainWindow);
     } // setupUi
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "Chaty", nullptr));
         textBrowser->setHtml(QCoreApplication::translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:'Source Sans Pro'; font-size:12pt; font-weight:600; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:'SimSun'; font-size:15pt; font-weight:400;\"><br /></p></body></html>", nullptr));
+        send->setText(QCoreApplication::translate("MainWindow", "Send", nullptr));
     } // retranslateUi
 
 };
